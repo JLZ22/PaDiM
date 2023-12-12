f7808e6f54d7e0138779bb4e2bdabd1c4b625278
larry <changyu.liu@alphabetter.com.cn>
2023-12-12 16:34:52 +0800

    Init upload

--- padim/engine/trainer.py
+++ padim/engine/trainer.py
@@ -59,10 +59,6 @@ class Trainer:
         return model
 
     def get_dataloader(self) -> torch.utils.data.DataLoader:
-        print((self.config.DATASETS.TRANSFORMS.RESIZE.HEIGHT, self.config.DATASETS.TRANSFORMS.RESIZE.WIDTH))
-        print((self.config.DATASETS.TRANSFORMS.CENTER_CROP.HEIGHT, self.config.DATASETS.TRANSFORMS.CENTER_CROP.WIDTH))
-        print(self.config.DATASETS.TRANSFORMS.NORMALIZE.MEAN)
-        print(self.config.DATASETS.TRANSFORMS.NORMALIZE.STD)
         train_dataset = MVTecDataset(
             self.config.DATASETS.ROOT,
             self.config.DATASETS.CATEGORY,
